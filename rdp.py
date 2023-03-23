29ca02888209b92c804c8eeb5b6286141710742e
Michael <30883926+mgalde@users.noreply.github.com>
2023-03-22 21:54:03 -0700

    Update rdp.py

--- rdp.py
+++ rdp.py
@@ -1,38 +1,31 @@
-""" 
-Copyright 2019 Michael Galde,
-file: rdp.py
-So this is used to monitor the network and look for conenctions on RDP port 3389
-"""
 import sys
 import argparse
+import logging
 import yagmail
 import datetime
 import time
 from socket import socket, AF_INET, SOCK_STREAM
 
-VERSION = '0.5 Mikes Fun Version'
+VERSION = '0.6 Mikes Fun Version'
 welcome = b"Secret Server login: "
-address = "localhost" #Change to your IP address 
 
-def send_email(src_address):
+def send_email(src_address, email_account, email_password, email_recipient):
     """ This sends a email from a gmail account so I am alerted """
     ts = time.time()
     st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
     contents = ("Port 3389 RDP was accessed by: " + (src_address) + " at: " + (st))
-    print (contents)
-    yagmail.SMTP('Your yagmail account').send('your email', 'HONEYPOT ALERT! - RDP', contents)
-    pass
+    logging.info(contents)
+    yagmail.SMTP(email_account, email_password).send(email_recipient, 'HONEYPOT ALERT! - RDP', contents)
 
-def rdp(address,port=3389):
+def rdp(address,port):
     """ RDP Service create a listening port """
     try:
         ski=socket(AF_INET,SOCK_STREAM)
         ski.bind((address, port))
         ski.listen()
         conn,addr = ski.accept()
-        print('ALERT! you have been visited by ' + addr[0])
-        send_email(addr[0])
-        """Send Alert Email"""
+        logging.warning('ALERT! you have been visited by ' + addr[0])
+        send_email(addr[0], args.email_account, args.email_password, args.email_recipient)
         conn.sendall(welcome)
         while True:
             data=conn.recv(1024)
@@ -42,6 +35,16 @@ def rdp(address,port=3389):
         ski.close()
         sys.exit()
 
-print("RDP monitor active")
-rdp(address)
-        
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Create a honeypot for RDP services.')
+    parser.add_argument('--address', type=str, default='localhost', help='IP address to listen on')
+    parser.add_argument('--port', type=int, default=3389, help='Port number to listen on')
+    parser.add_argument('--email_account', type=str, help='Email account for sending alerts')
+    parser.add_argument('--email_password', type=str, help='Email account password for sending alerts')
+    parser.add_argument('--email_recipient', type=str, help='Email recipient for sending alerts')
+    args = parser.parse_args()
+
+    logging.basicConfig(filename='rdp_honeypot.log', level=logging.WARNING, format='%(asctime)s %(levelname)s %(message)s')
+    logging.warning('RDP monitor active')
+
+    rdp(args.address, args.port)
